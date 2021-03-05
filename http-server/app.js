482f8d62954c257b8a81afb32fe545e7ecb15953
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-03-05 21:33:00 +0200

    finish lesson

---
+++ http-server/app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+http.createServer((request, response)=> {
+    // console.log(`URL: ${request.url}`);
+    // console.log(`METHOD: ${request.method}`);
+    // console.log(`Headers: ${request.headers}`);
+    
+    response.setHeader("Content-Type", "text/html; charset=utf-8")
+    response.write("<h2>Добро пожаловать на сайт!</h2>");
+    response.end();
+}).listen(3000, ()=> console.log("It's work!"));
